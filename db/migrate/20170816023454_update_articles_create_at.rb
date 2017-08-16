23213ac956a354e2f11cd91c97daa3245a634cfc
Matthew Sevey <mjsevey@gmail.com>
2017-08-15 22:38:16 -0400

    updated articles db and displayed user info on articles

---
+++ db/migrate/20170816023454_update_articles_create_at.rb
@@ -0,0 +1,5 @@
+class UpdateArticlesCreateAt < ActiveRecord::Migration[5.1]
+  def change
+  	rename_column :articles, :create_at, :created_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816015119) do
+ActiveRecord::Schema.define(version: 20170816023454) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "create_at"
+    t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
   end
